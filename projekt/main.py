2c3bcf7302748c4b8d83a1c0f0430f66ea0be8bc
GreenFace28 <mvlach@seznam.cz>
2025-06-20 21:00:46 +0200

    zkouska

---
+++ projekt/main.py
@@ -0,0 +1,10 @@
+"""
+projekt_1.py: první projekt do Engeto Online Python Akademie
+
+author: Michal Vlach
+email: mvlach@seznam.cz
+"""
+# přihlášení
+users = ("bob" , "amanda", "tata")
+
+
---
+++ zkousky/main.py
@@ -0,0 +1,5 @@
+vyska = 2
+vaha = 50
+jmeno = "Jana"
+bmi = vaha/vyska**2
+print("Jméno:" + jmeno + "BMI:" + str(bmi))

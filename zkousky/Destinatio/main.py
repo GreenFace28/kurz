a0ad733be978e5b40a7a943bd986df558e41fc7f
GreenFace28 <mvlach@seznam.cz>
2025-06-25 21:06:29 +0200

    prace na Destinatio

---
+++ zkousky/Destinatio/main.py
@@ -0,0 +1,26 @@
+mesta = ["Praha" , "Viden" , "Olomouc" , "Svitavy" , "Zlín" , "Ostrava"]
+ceny = (150, 200, 120, 120, 100, 180)
+cara = "=" * 35
+nabidka = """
+1 - Praha   | 150,-Kč
+2 - Viden   | 200,-Kč
+3 - Olomouc | 120,-Kč
+4 - Svitavy | 120,-Kč
+5 - Zlin    | 100,-Kč
+6 - Ostrava | 180,-Kč
+"""
+
+print("Vítejte v naší aplikaci Destinatio")
+print(cara)
+
+print(nabidka)
+print(cara)
+
+destinace = input("Zadej číslo destinace:")
+jmeno = input("Zadej svoje jméno:")
+prijmeni = input("Zadej prijmení:")
+email = input("Zadej email:")
+
+print(cara)
+
+print(f"Zadal jsi: /n Destinace={destinace} /n Jméno=)
--- zkousky/main.py
+++ zkousky/main.py
@@ -1,5 +1,1 @@
-vyska = 2
-vaha = 50
-jmeno = "Jana"
-bmi = vaha/vyska**2
-print("Jméno:" + jmeno + "BMI:" + str(bmi))
+print(type(int(input("Zadej číselný znak: "))))

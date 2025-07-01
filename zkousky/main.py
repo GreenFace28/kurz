2c02ce137aaba5a241c50d92a9c5df0af42a734c
GreenFace28 <mvlach@seznam.cz>
2025-07-01 21:16:46 +0200

    pokročení v projektu, doplnění počítání písmen počítání čísel a velkych a malych slov

--- zkousky/main.py
+++ zkousky/main.py
@@ -1,1 +1,12 @@
-print(type(int(input("Zadej číselný znak: "))))
+jmeno = "Marek"
+heslo = "12364"
+uzivatel = {"Marek": "1234"}
+zprava = ()
+
+if jmeno in uzivatel and heslo in uzivatel.get("Marek"):
+  zprava = "Ahoj Marek, vítej v aplikaci! Pokračuj..."
+else:
+  zprava = "Uživatelské jméno nebo heslo nejsou v pořádku!"
+
+print(zprava)
+  
